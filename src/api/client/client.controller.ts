eff141562fadfef6ed71e5c80fb7f5c7b067d6f8
Jose Acevedo <josearmandoacevedoangarita@gmail.com>
2020-05-09 19:37:01 -0500

    Configurando guardas en las rutas de clientes

--- src/api/client/client.controller.ts
+++ src/api/client/client.controller.ts
@@ -43,7 +43,7 @@ export class ClientController {
     } catch (error) {
       return res
         .status(HttpStatus.FOUND)
-        .json({ message: 'Client does not exists' });
+        .json({ message: 'Client does not existss' });
     }
   }
 
